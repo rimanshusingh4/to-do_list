cb7cbfe6bb8ee3193579bb966c0d4f2afb8e1ab0
rimanshusingh4 <rimanshusingh4@gmail.com>
2023-12-13 17:39:25 +0530

    Third

--- components/NoteList.jsx
+++ components/NoteList.jsx
@@ -70,7 +70,7 @@ const NoteList = ({ notes, setNotes }) => {
                             <a className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                             
                                {
-                                    editNoteId === -1 ? <button disabled onClick={() => handleEdit(id)}>Edit</button> :
+                                    editNoteId === -1 ? <button onClick={() => handleEdit(id)}>Edit</button> :
                                         <button onClick={updateNote}>Update</button>
                                }
                                 
